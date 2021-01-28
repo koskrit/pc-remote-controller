9d2f781da3e2f32616079403e4daea6173c736a7
koskrit <koskritm@gmail.com>
2021-01-28 22:30:01 +0200

    formated server.js

--- server.js
+++ server.js
@@ -1,55 +1,46 @@
 //npm i express
 
 const express = require("express");
-
 const robot = require("kbm-robot");
-
 const cors = require("cors");
 
 const app = express();
 const PORT = process.env.PORT || 4000;
 app.use(cors());
 
-
-
 app.get("/space", (req, res) => {
-   
-
    robot.startJar();
 
-   robot.press("SPACE")
-       
-       .sleep(100)
-       
-       .release("SPACE")
-       .sleep(100)
-      
-       .go()
-       .then(robot.stopJar);
+   robot
+      .press("SPACE")
+
+      .sleep(100)
+
+      .release("SPACE")
+      .sleep(100)
+
+      .go()
+      .then(robot.stopJar);
 
    res.json({ message: "done" });
 });
 app.get("/tab", (req, res) => {
-  
    robot.startJar();
 
-robot.press("ctrl")
-    .press("tab")
-    .sleep(100)
-    .release("tab")
-    .release("ctrl")
-    .sleep(100)
-    
-    .go()
-    .then(robot.stopJar);
+   robot
+      .press("ctrl")
+      .press("tab")
+      .sleep(100)
+      .release("tab")
+      .release("ctrl")
+      .sleep(100)
+
+      .go()
+      .then(robot.stopJar);
 
    res.json({ message: "done" });
 });
 
-
-
 app.listen(PORT, () => {
    console.log("listening on http://localhost:4000");
 });
-
-let os = require('os')
