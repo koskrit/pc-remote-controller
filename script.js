b0fbd75fe15bc5274823716dd5dc849868daae78
koskrit <koskritm@gmail.com>
2021-01-28 22:22:25 +0200

    removed ip adress

--- script.js
+++ script.js
@@ -6,14 +6,14 @@ let tab =document.querySelector('#switch-tab')
 pause.addEventListener('click',async () => {
 
 
-let raw = await fetch('http://192.168.1.10:4000/space')
+let raw = await fetch("host-adress"+ '/space')
 let result = await raw.json()
 console.log(result);
 })
 let tab =document.querySelector('#switch-tab')
 tab.addEventListener('click',async () => {
 
-  let raw = await fetch('http://192.168.1.10:4000/tab')
+  let raw = await fetch("host-adress"+ '/tab')
   let result = await raw.json()
   console.log(result);
   })
